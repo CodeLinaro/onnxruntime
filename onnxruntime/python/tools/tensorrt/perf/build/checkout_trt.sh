363c87c22d79aaf301d5dcda14c956da19b95d84
Olivia Jain <oljain@microsoft.com>
2022-01-12 18:38:16 -0800

    point to correct 8.0 commit

--- onnxruntime/python/tools/tensorrt/perf/build/checkout_trt.sh
+++ onnxruntime/python/tools/tensorrt/perf/build/checkout_trt.sh
@@ -8,7 +8,7 @@ then
     git remote update
     if [ "$1" = "8.0" ]
     then 
-        git checkout "$1"'-GA'
+        git checkout 1f416bb462689f3ef9e3f1057a113d9c6aba6972
     fi
     if [ "$1" = "7.2" ]
     then 
